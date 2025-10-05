cd556a72669aa7b7db03d7ff4d6405afc66e201b
agolys <alexandergolys@gmail.com>
2025-10-05 03:14:04 +0200

    separated string utils to different header

--- utils/stringUtils.hpp
+++ utils/stringUtils.hpp
@@ -3,11 +3,10 @@
 #include "concepts.hpp"
 #include "abstractNonsense.hpp"
 
-inline string removeWhitespace(const string &s){
+inline string removeWhitespace(const string &s) {
 	string res;
-	for (char c : s)
-		if (!isspace(c))
-			res += c;
+	for (char c: s)
+		if (!isspace(c)) res += c;
 	return res;
 }
 
@@ -15,29 +14,33 @@ inline string removeWhitespace(const string &s){
 
 
 inline string vecToString(vec2 v) {
-	return "(" + to_string(v.x) + ", " + to_string(v.y) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ")";
+}
 
 inline string vecToString(vec3 v) {
-	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ")";
+}
 
 inline string vecToString(vec4 v) {
-	return  "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ", " + to_string(v.w) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ", " + to_string(v.w) + ")";
+}
 
 inline string vecToString(ivec2 v) {
-	return "(" + to_string(v.x) + ", " + to_string(v.y) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ")";
+}
 
 inline string vecToString(ivec3 v) {
-	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ")";
+}
 
 inline string vecToString(ivec4 v) {
-	return  "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ", " + to_string(v.w) + ")"; }
+	return "(" + to_string(v.x) + ", " + to_string(v.y) + ", " + to_string(v.z) + ", " + to_string(v.w) + ")";
+}
 
 template<typename T>
-void printVector(vector<T> v, string title="vector")
-{
+void printVector(vector<T> v, string title = "vector") {
 	std::cout << title << " [" << v.size() << "]: ";
-	for (int i = 0; i < v.size(); i++)
-	{
+	for (int i = 0; i < v.size(); i++) {
 		std::cout << v[i] << ", ";
 	}
 	std::cout << std::endl;
@@ -61,4 +64,29 @@ inline vector<string> split(const string &s, const string &delim) {
 
 inline vector<string> split(const string &s, char delim) {
 	return split(s, string(1, delim));
+}
+
+inline string replace(const string &s, const string &from, const string &to) {
+	string res = s;
+	size_t start_pos = 0;
+
+	while ((start_pos = res.find(from, start_pos)) != string::npos) {
+		res.replace(start_pos, from.length(), to);
+		start_pos += to.length();
+	}
+	return res;
+}
+
+inline string replaceRecursive(const string &s, const string &from, const string &to) {
+	string res = replace(s, from, to);
+	if (res == s) return res;
+	return replaceRecursive(res, from, to);
+}
+
+inline string remove(const string &s, const string &key) {
+	return replace(s, key, "");
+}
+
+inline string removeRecursive(const string &s, const string &key) {
+	return replaceRecursive(s, key, "");
 }
